55c4dad88789c46103755652f0fedecaeac6c5cc
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-06-15 14:46:55 +0530

    Next Greater Element

--- STACKS/StockApanProb.cpp
+++ STACKS/StockApanProb.cpp
@@ -1,39 +1,87 @@
 
+// #include<iostream>
+// #include<stack>
+// #include<vector>
+
+// using namespace std;
+
+// void stockspanProblem(vector<int> stock, vector<int> span){
+//     stack<int>s;
+//     s.push(0);
+//     span[0]=1;
+
+//     for(int i=1;i<stock.size(); i++){
+//         int currPrice=stock[i];
+//         while(!s.empty() && currPrice>=stock[s.top()]){
+//             s.pop();
+//         }
+//         if(s.empty()){
+//             span[i]=i+1;
+//         }else{
+//             int prevHigh=s.top();
+//             span[i]=i-prevHigh;
+//         }
+//     }
+//  for(int i=0;i<span.size();i++){
+//     cout<<span[i]<<" ";
+//  }
+
+//  cout<<endl;
+// }
+
+
+// int main(){
+
+//     vector<int> stock={100, 80,60,70,60,85,100};
+//     vector<int>span={0,0,0,0,0,0,0};
+//     stockspanProblem(stock,span);
+//     return 0;
+// }
+
+
+
+
+//Next Greater Element
+
+//next Greater=first greater element that is to the right
+
 #include<iostream>
 #include<stack>
+#include<string>
 #include<vector>
 
 using namespace std;
 
-void stockspanProblem(vector<int> stock, vector<int> span){
+void nextGreater(vector<int> arr, vector<int>ans){
     stack<int>s;
-    s.push(0);
-    span[0]=1;
+    int idx=arr.size()-1;
 
-    for(int i=1;i<stock.size(); i++){
-        int currPrice=stock[i];
-        while(!s.empty() && currPrice>=stock[s.top()]){
+    ans[idx]=-1;
+    s.push(arr[idx]);
+
+    for(idx=idx-1;idx>=0;idx--){
+        int curr=arr[idx];
+        while(!s.empty() && curr>=s.top()){
             s.pop();
         }
         if(s.empty()){
-            span[i]=i+1;
+            ans[idx]=-1;
         }else{
-            int prevHigh=s.top();
-            span[i]=i-prevHigh;
+            ans[idx]=s.top();
         }
+        s.push(curr);
     }
- for(int i=0;i<span.size();i++){
-    cout<<span[i]<<" ";
- }
-
- cout<<endl;
+    for(int i=0;i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
 }
-
 
 int main(){
 
-    vector<int> stock={100, 80,60,70,60,85,100};
-    vector<int>span={0,0,0,0,0,0,0};
-    stockspanProblem(stock,span);
+vector<int> arr={6,8,0,1,3};
+vector<int>ans={0,0,0,0,0};
+nextGreater(arr, ans);
+
     return 0;
 }
