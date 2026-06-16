6d33e551f671d37af8e57b68cd6a5b5036f2d82b
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-06-16 17:07:53 +0530

    Tree traversals preorder, inorder , and postorder

--- BinaryTrees/PreOrderTree.cpp
+++ BinaryTrees/PreOrderTree.cpp
@@ -39,12 +39,62 @@ Node* buildTree(vector<int> nodes){
 
 }
 
+//Tree Traversals
+
+//preorder traversal
+
+void preorder(Node* root){ //O(n)
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+//inorder traversal
+
+void inorder(Node* root){  //O(n)
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+
+}
+
+//postorder traversal
+//left subtree
+//right subtree
+//root
+
+void postorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
 int main(){
 
 vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 
 Node* root=buildTree(nodes); //1
 cout<<"root ="<<root->data<<endl;
+
+cout<<"preorder traversal: "<<endl;
+preorder(root);
+cout<<endl;
+
+cout<<"inorder traversal: "<<endl;
+inorder(root);
+cout<<endl;
+
+cout<<"postorder traversal: "<<endl;
+postorder(root);
 
     return 0;
 }
