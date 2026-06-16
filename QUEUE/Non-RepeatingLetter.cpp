ac9dcd198e9d2a316c85b50732199628e28670e6
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-06-16 12:11:58 +0530

    First Non-Repeating Letter Problem

---
+++ QUEUE/Non-RepeatingLetter.cpp
@@ -0,0 +1,38 @@
+
+//Non-Repeating Letter Problem
+
+//Find first non-repeating letter in a stream of characters
+//aabccxb
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void FirstNonRepeat(string str){  //O(n)
+    queue<char> Q;
+    int freq[26]={0};
+
+    for(int i=0;i<str.size(); i++){
+        char ch=str[i];
+        Q.push(ch);
+        freq[ch-'a']++;
+
+        while(!Q.empty() && freq[Q.front()-'a']>1){
+            Q.pop();
+        }
+        if(Q.empty()){
+            cout<<"-1\n";
+        }else{
+            cout<<Q.front()<<"\n";
+        }
+    }
+}
+
+int main(){
+
+string str="aabccxb";
+
+FirstNonRepeat(str);
+
+    return 0;
+}
